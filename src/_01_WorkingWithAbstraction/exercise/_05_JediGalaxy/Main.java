602224e4422401f0f1b7f4056756ad7bd5e86bf4
VGoychev <vladimirgoichev@gmail.com>
2024-10-23 19:48:15 +0300

    Initial commit

--- src/_01_WorkingWithAbstraction/exercise/_05_JediGalaxy/Main.java
+++ src/_01_WorkingWithAbstraction/exercise/_05_JediGalaxy/Main.java
@@ -4,61 +4,74 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    
+    private static long sum = 0;
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] dimestions = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int x = dimestions[0];
-        int y = dimestions[1];
-
-        int[][] matrix = new int[x][y];
-
-        int value = 0;
-        for (int i = 0; i < x; i++)
-        {
-            for (int j = 0; j < y; j++)
-            {
-                matrix[i][j] = value++;
-            }
-        }
-
+        int[][] matrix = fillMatrix(scanner);
         String command = scanner.nextLine();
-        long sum = 0;
-        while (!command.equals("Let the Force be with you"))
-        {
-            int[] ivoS = Arrays.stream(command.split(" ")).mapToInt(Integer::parseInt).toArray();
-            int[] evil = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int xE = evil[0];
-            int yE = evil[1];
 
-            while (xE >= 0 && yE >= 0)
-            {
-                if (xE >= 0 && xE < matrix.length && yE >= 0 && yE < matrix[0].length)
-                {
-                    matrix[xE] [yE] = 0;
-                }
-                xE--;
-                yE--;
-            }
+        while (!command.equals("Let the Force be with you")) {
+            int[] playerCoordinates = Arrays.stream(command.split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            int[] evilCoordinates = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
 
-            int xI = ivoS[0];
-            int yI = ivoS[1];
+            int evilX = evilCoordinates[0];
+            int evilY = evilCoordinates[1];
 
-            while (xI >= 0 && yI < matrix[1].length)
-            {
-                if (xI >= 0 && xI < matrix.length && yI >= 0 && yI < matrix[0].length)
-                {
-                    sum += matrix[xI][yI];
-                }
+            moveEvil(evilX, evilY, matrix);
 
-                yI++;
-                xI--;
-            }
+            int playerX = playerCoordinates[0];
+            int playerY = playerCoordinates[1];
+
+            movePlayer(matrix, playerX, playerY);
 
             command = scanner.nextLine();
         }
 
         System.out.println(sum);
 
+    }
+
+    private static void movePlayer(int[][] matrix,int row, int col) {
+        while (row >= 0 && col < matrix[1].length) {
+            if (row < matrix.length && col >= 0) {
+                sum += matrix[row][col];
+            }
+
+            col++;
+            row--;
+        }
+    }
+
+    private static void moveEvil(int row, int col, int[][] matrix) {
+        while (row >= 0 && col >= 0) {
+            if (row < matrix.length && col < matrix[0].length) {
+                matrix[row] [col] = 0;
+            }
+            row--;
+            col--;
+        }
+    }
+
+    private static int[][] fillMatrix(Scanner scanner) {
+        int[] dimensions = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int x = dimensions[0];
+        int y = dimensions[1];
+        int[][] matrix = new int[x][y];
+        int value = 0;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                matrix[i][j] = value++;
+            }
+        }
+        return matrix;
     }
 }
