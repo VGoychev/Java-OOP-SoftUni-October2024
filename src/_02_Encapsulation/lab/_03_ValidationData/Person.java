6e9313a1ff12d1920e27ce40a2e5df7863a1aa59
VGoychev <vladimirgoichev@gmail.com>
2024-10-25 22:53:56 +0300

    Initial commit

---
+++ src/_02_Encapsulation/lab/_03_ValidationData/Person.java
@@ -0,0 +1,78 @@
+package _02_Encapsulation.lab._03_ValidationData;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAge(age);
+        setSalary(salary);
+    }
+
+    public double getSalary() {
+
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 460.0){
+            throw new IllegalArgumentException("Salary cannot be less than 460.0 leva");
+        } else {
+            this.salary = salary;
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName.length() < 3){
+            throw new IllegalArgumentException("First name cannot be less than 3 symbols");
+        } else {
+            this.firstName = firstName;
+        }
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.length() < 3){
+            throw new IllegalArgumentException("Last name cannot be less than 3 symbols");
+
+        } else {
+            this.lastName = lastName;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age <= 0) {
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        } else {
+            this.age = age;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %.1f leva", getFirstName(), getLastName(), getSalary());
+    }
+
+    public void increaseSalary(double bonus){
+        if (this.getAge() < 30){
+            this.setSalary(this.getSalary() + (this.getSalary() * bonus / 200));
+        } else {
+            this.setSalary(this.getSalary() + (this.getSalary() * bonus / 100));
+        }
+    }
+}
